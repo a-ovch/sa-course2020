f8f6fd558f9404a368b43269aa4ce4f2ef104cec
Andrey Ovchinnikov <a.ovch90@gmail.com>
2020-04-11 17:07:55 +0300

    GET /health has been implemented

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+const serverAddr = ":8000"
+
+func healthHandler(w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprint(w, "{\"status\": \"OK\"}")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/health", healthHandler).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(serverAddr, r))
 }
