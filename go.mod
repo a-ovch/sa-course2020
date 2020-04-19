fbc31267079b9d0c43c93b7a096720854b7c8a60
Andrey Ovchinnikov <a.ovch90@gmail.com>
2020-04-19 21:57:58 +0300

    HTTP server graceful shutdown has been implemented You should use SERVER_PORT environment variable to set the application port

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module healthcheck
+module users
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require github.com/gorilla/mux v1.7.4 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,70 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/gorilla/mux"
 )
 
-const serverAddr = ":8000"
+const portEnvVar = "SERVICE_PORT"
 
 func healthHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
 	_, _ = fmt.Fprint(w, "{\"status\": \"OK\"}")
 }
 
-func main() {
+func httpLogMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v %v\n", r.Method, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func startHttpServer() *http.Server {
 	r := mux.NewRouter()
 	r.HandleFunc("/health", healthHandler).Methods(http.MethodGet)
-	log.Fatal(http.ListenAndServe(serverAddr, r))
+
+	port := os.Getenv(portEnvVar)
+	if _, err := strconv.Atoi(port); err != nil {
+		log.Fatalf("Can not use \"%v\" as HTTP port.", port)
+	}
+
+	s := &http.Server{
+		Addr:    ":" + port,
+		Handler: httpLogMiddleware(r),
+	}
+
+	log.Printf("Server successfully started on %v port\n", port)
+
+	go func() {
+		log.Fatal(s.ListenAndServe())
+	}()
+
+	return s
+}
+
+func main() {
+	osSignalChan := make(chan os.Signal, 1)
+	signal.Notify(osSignalChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	srv := startHttpServer()
+
+	sig := <-osSignalChan
+	log.Printf("OS signal received: %+v", sig)
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %+v", err)
+	}
+
+	log.Print("Server shutdown successfully!")
 }
